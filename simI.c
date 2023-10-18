65073babf42d8bb7bb642da8572d10bdeef63316
kangrot <kangrotnengak@gmail.com>
2023-10-17 21:45:59 -0700

    change

--- simI.c
+++ simI.c
@@ -29,7 +29,7 @@ char *read_incom()
 		write(STDOUT_FILENO, "\n", 2);
 		return (NULL);
 	}
-	/* prompt on newline if user taps on only enter, or spaces */
+	/* prompts on the newline if user taps on only enter, or spaces */
 	if (linebuff[0] == '\n' || (strspn(linebuff, " \t\r\n") ==
 				strlen(linebuff)))
 	{
