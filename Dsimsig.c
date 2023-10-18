1c4f68123d12c4ec5b97995260053b6401e4db73
kangrot <kangrotnengak@gmail.com>
2023-10-17 21:48:12 -0700

    signal

--- Dsimsig.c
+++ Dsimsig.c
@@ -1,7 +1,7 @@
 #include "simshell.h"
 
 /**
- * ctrl_d_handler - Signal handler for CTRL+D (EOF)
+ * ctrl_d_handler - handles signals for CTRL+D (EOF)
  * @signum: signal integer
  */
 void ctrl_d_handler(int signum)
